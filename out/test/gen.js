acc5f055b887229b3dbd09224860d7ac20f87924
Nico Vergauwen <vergauwennico@gmail.com>
2019-04-03 11:11:23 +0200

    remove out

--- out/test/gen.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../soldoc/index");
-const path = require("path");
-const file = path.resolve(__dirname, './bugsbunny.sol');
-index_1.generateCommentsFromFile(file);
